0a47b89e18fbfe207d105c0a2c1bb7f5146a8dd5
cookiemovies <martin@kochloefl.de>
2021-02-21 20:57:07 +0100

    changes in index

---
+++ index.js
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+const express = require("express");
+const msal = require('@azure/msal-node');
+
+const SERVER_PORT = process.env.PORT || 3000;
+
+// Before running the sample, you will need to replace the values in the config, 
+// including the clientSecret
+const config = {
+    auth: {
+        clientId: "6658326c-5b15-44ab-b1eb-d1ca842f44fb",
+        authority: "https://login.microsoftonline.com/common",
+        clientSecret: "J_fiUL~sJ.w1V9b4_C713Dyl_A_8qL4uAb"
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback(loglevel, message, containsPii) {
+                console.log(message);
+            },
+            piiLoggingEnabled: false,
+            logLevel: msal.LogLevel.Verbose,
+        }
+    }
+};
+
+// Create msal application object
+const cca = new msal.ConfidentialClientApplication(config);
+
+// Create Express App and Routes
+const app = express();
+
+app.get('/', (req, res) => {
+    const authCodeUrlParameters = {
+        scopes: ["user.read"],
+        redirectUri: "http://localhost:3000/redirect",
+    };
+
+    // get url to sign user in and consent to scopes needed for application
+    cca.getAuthCodeUrl(authCodeUrlParameters).then((response) => {
+        res.redirect(response);
+    }).catch((error) => console.log(JSON.stringify(error)));
+});
+
+app.get('/redirect', (req, res) => {
+    const tokenRequest = {
+        code: req.query.code,
+        scopes: ["user.read"],
+        redirectUri: "http://localhost:3000/redirect",
+    };
+
+    cca.acquireTokenByCode(tokenRequest).then((response) => {
+        console.log("\nResponse: \n:", response);
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).send(error);
+    });
+});
+
+
+app.listen(SERVER_PORT, () => console.log(`Msal Node Auth Code Sample app listening on port ${SERVER_PORT}!`))
